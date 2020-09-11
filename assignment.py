4092763dc2e93cdc0def3f2e7a140dc6f960a46e
Hitasvi <patelhitasvi@gmail.com>
2020-09-10 21:37:59 -0400

    assignment completed

---
+++ assignment.py
@@ -0,0 +1,12 @@
+userNumber = input("What is your favorite number? ")
+print("My Favorite Number is", userNumber)
+
+if userNumber == "8":
+    print("You are pass")
+elif userNumber == "23":
+    print("You got secret message")
+else:
+    print("You are fail")
+
+shouting = "you shall not pass"
+print(shouting.upper())
