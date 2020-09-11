bc5f8bb3660c941efa2e90a037359fa18e1e8a41
Hitasvi <patelhitasvi@gmail.com>
2020-09-10 21:42:00 -0400

    assignment completed

---
+++ stringmethods.py
@@ -0,0 +1,13 @@
+ourString = "This is our string"
+lengthofstring = len(ourString)
+print(lengthofstring)
+
+print(ourString.upper())
+
+print(ourString.lower())
+
+print(ourString.title())
+
+email_Introduction = "Hello My Name is {} {}"
+
+print(email_Introduction.format("Hitu", "Patel"))
