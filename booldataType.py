21bfd1d00e5c7f72795474c34d1cfc4b0a1b13da
Hitasvi <patelhitasvi@gmail.com>
2020-09-10 21:38:30 -0400

    assignment completed

---
+++ booldataType.py
@@ -0,0 +1,8 @@
+#1 will always show true
+print(bool(1))
+#0 will always show false
+print(bool(0))
+#Blank string is always false
+print(bool(""))
+#anythins inside string is always true
+print(bool("Hii"))
